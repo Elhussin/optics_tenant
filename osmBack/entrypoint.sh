8cc33e7025032c9d864a4d21312a6d9135c9e37e
Elhussin <hasin3112@gmail.com>
2026-01-03 01:47:38 +0300

    git status

--- osmBack/entrypoint.sh
+++ osmBack/entrypoint.sh
@@ -17,7 +17,7 @@ retry = 0
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "db")
+DB_HOST = os.getenv("DB_HOST", "tenant_db")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
 while retry < max_retries:
